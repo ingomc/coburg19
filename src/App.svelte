62de59bad79ca1096995de28e991a20da9bb9541
Andre Bellmann <andre.bellmann@huk-coburg.de>
2020-10-21 12:25:31 +0200

    update styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,41 @@
 <script>
-	export let data;
+  export let data;
+  let warningclass = "yellow";
+
+  if (data.cases7_per_100k < 35) {
+    warningclass = "green";
+  }
+
+  if (data.cases7_per_100k >= 50) {
+    warningclass = "red";
+  }
 </script>
 
-<main>
-	<h3><strong>7-Tage Inzidenz pro 100.000 Einwohner:</strong></h3>
-	<h1>{data.cases7_per_100k}</h1>
-	<h2>Bezirk: {data.BEZ},&nbsp;{data.GEN}</h2>
-	<p><small>Zuletzt aktualisiert: {data.last_update}</small></p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 100%;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    display: inline-block;
+    color: var(--inzidenz);
+    font-size: 4em;
+    font-weight: 400;
+    padding: 0.5rem 2rem;
+    border-radius: 4px;
+    background-color: var(--bg);
+  }
 </style>
+
+<main class={warningclass}>
+  <h3>
+    <strong>7-Tage Inzidenz pro 100.000 Einwohner:</strong>
+  </h3>
+  <h1>{Number(data.cases7_per_100k).toFixed(2)}</h1>
+  <h2>Bezirk: {data.BEZ},&nbsp;{data.GEN}</h2>
+  <p>
+    <small>Zuletzt aktualisiert: {data.last_update}</small>
+  </p>
+</main>
