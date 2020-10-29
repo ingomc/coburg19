8eb96e4545c8ebf90807f198f19042ad3103f986
Andre <andre@MacBook-Pro.local>
2020-10-29 23:29:58 +0100

    format

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import App from './App.svelte';
 import { locations } from '../generated/data.json';
 
-
 const app = new App({
-	target: document.body,
-	props: {
-		data: locations
-	}
+  target: document.body,
+  props: {
+    data: locations,
+  },
 });
 
 export default app;
